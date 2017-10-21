78bef51ecf2128be3e0f9737d0bcb5c42241ba32
nightlifex <steven222giese@gmail.com>
2017-10-21 23:05:40 +0200

    fff

--- js/config.js
+++ js/config.js
@@ -71,7 +71,7 @@ var l_bgOverlay = true;
 
 /*Background darkening amount
 0 for none (0%), 100 for pitch black (100%)*/
-var l_bgDarkening = 55;
+var l_bgDarkening = 50;
 
 /*Music player
 'true' or 'false' to enable music in the background*/
@@ -92,7 +92,7 @@ var l_musicRandom = true;
 
 /*Music volume
 Choose a value between 0 and 100*/
-var l_musicVolume = 20;
+var l_musicVolume = 10;
 
 /*Enable custom messages?*/
 var l_messagesEnabled = true;
