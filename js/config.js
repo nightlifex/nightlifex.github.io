66eb4ea650b150566afe52e9da13b37ba5929510
nightlifex <steven222giese@gmail.com>
2017-11-01 19:20:23 +0100

    f_f
    f

--- js/config.js
+++ js/config.js
@@ -92,7 +92,7 @@ var l_musicRandom = true;
 
 /*Music volume
 Choose a value between 0 and 100*/
-var l_musicVolume = 5;
+var l_musicVolume = 1;
 
 /*Enable custom messages?*/
 var l_messagesEnabled = true;
